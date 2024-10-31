7ddcac6154fb866fd2de20e853a87eecd9a641d5
MarioCaushi <caushimario321@gmail.com>
2024-10-31 01:32:29 +0100

    Added functionality to register.html and made some changes to index.js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,34 @@
-//Declaring lists for clients and book to store and use throughout the application
-let client=[];
-let book=[];
-let manager=null; //there is only one
-
-//Fetching the Data from the JSON file and seperating it into lists for easier access
+// Fetching the Data from the JSON file and separating it into lists for easier access
 function fetchData() {
     fetch("json/data.json")
     .then(response => {
-        if(!response.ok)
-            {
-                throw new Error("Fetching data from JSON file went wrong");
-            }
-            return response.json();
+        if (!response.ok) {
+            throw new Error("Fetching data from JSON file went wrong");
+        }
+        return response.json();
     })
     .then(data => {
         console.log(data);
-        
-        manager=data.manager;
-        client=data.clients;
-        book=data.books;
 
-        //Storing the lists in local-storage so thet can be easily accessed throughout different files
-        //and updated so the content is not lost when page loaded or whatever
-
-        localStorage.setItem("client",JSON.stringify(client));
-        localStorage.setItem("book",JSON.stringify(book));
-
+        // Storing the data in local storage
+        localStorage.setItem("client", JSON.stringify(data.clients));
+        localStorage.setItem("book", JSON.stringify(data.books));
+        localStorage.setItem("manager",JSON.stringify(data.manager))
     })
-    .catch(Error => {
+    .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
     });
-};
+}
 
-//Function to evaluate the login-form input 
+// Function to evaluate the login-form input 
 function evaluateLogin() {
     let username = document.getElementById("username-login").value;
     let password = document.getElementById("password-login").value;
-    console.log(username)
-    console.log(password)
-
+    
     document.getElementById("username-login").value = "";
     document.getElementById("password-login").value = "";
+
+    manager=JSON.parse(localStorage.getItem("manager"));
 
     if (username === manager.Username) {
         if (manager.Password !== password) {
@@ -50,11 +38,9 @@ function evaluateLogin() {
         }
         //If manager credentials added successfull then you need to redirct to your own page
         console.log("Manager logged in successfully!");
-        localStorage.setItem("manager",JSON.stringify(manager));
-        window.location.href="manager-welcome.html";
-
+        window.location.href = "manager-welcome.html";
     } else {
-        
+        client=JSON.parse(localStorage.getItem("client"));
         const user = client.find(user => user.Username === username);
         if (user) {
             if (user.Password !== password) {
@@ -62,20 +48,20 @@ function evaluateLogin() {
                 document.getElementById("login-evaluation").innerHTML = "<p class='text-danger small'>Password not correct!</p>";
                 return; 
             }
-              //If user credentials added successfull then you need to redirct to your own page
-              console.log("User logged in successfully!");
-              localStorage.setItem("user",JSON.stringify(user));
-              window.location.href="client-welcome.html";
-
+            //If user credentials added successfull then you need to redirct to your own page
+            console.log("User logged in successfully!");
+            localStorage.setItem("user", JSON.stringify(user));
+            window.location.href = "client-welcome.html";
         } else {
             document.getElementById("login-evaluation").innerHTML = "<p class='text-danger small'>User does not exist!</p>";
         }
     }
 }
 
+// Fetching the actual data
+if (localStorage.length === 0) {
+    fetchData();
+} 
 
-//Fetching the actual data
-fetchData();
-
-//Evaluating the login input
+// Evaluating the login input
 document.getElementById("login-btn").addEventListener("click", evaluateLogin);
