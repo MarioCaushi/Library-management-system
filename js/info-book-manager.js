c75c4399a0b86d3960808997440377c444b2ae0e
MarioCaushi <caushimario321@gmail.com>
2025-01-23 19:14:49 +0100

    Finished info-book page

--- js/info-book-manager.js
+++ js/info-book-manager.js
@@ -1,8 +1,7 @@
 import { logoutAction, selectBook, deleteBookAPI } from "./manager-book-management.js";
 
 //Function to get BookInfo fromm the API
-async function getBookInfoAPI(id)
-{
+async function getBookInfoAPI(id) {
     const url = `http://localhost:5223/Book/get-book-info/${id}`;
     try {
         const response = await fetch(url, {
@@ -24,13 +23,12 @@ async function getBookInfoAPI(id)
 }
 
 //Function to show the info of a specific book when the info button is clicked ==> The MAIN function of this file
-async function showBookInfo()
-{
+async function showBookInfo() {
     const bookInfoId = JSON.parse(localStorage.getItem("selectedBook"));
 
     const bookInfo = await getBookInfoAPI(bookInfoId);
 
-    if(!bookInfo) {
+    if (!bookInfo) {
         console.log("No bookInfo element from info-book-management file");
         return;
     }
@@ -39,15 +37,9 @@ async function showBookInfo()
 
     detailsBookInfo(bookInfo);
 
-    const clientLikes = bookInfo["noLikes"];
-    console.log("Book-likes length: ", clientLikes);
+    showBookLikes(bookInfo["bookLikes"], "info");
 
-    showBookLikes(clientLikes,bookInfo,"info");
-
-    const clientReviews =  bookInfo["noReviews"];
-    console.log("Book-reviews length: ", clientReviews);
-
-    showBookReviews(clientReviews, bookInfo, "info");
+    showBookReviews(bookInfo["bookReviews"], "info");
 
 };
 
@@ -55,20 +47,21 @@ async function showBookInfo()
 //Other complementary functions to the one above
 
 //Function to deal with the WHOLE client-review part of the Book
-function showBookReviews(clientReviews, bookInfo, keyword) {
+function showBookReviews(clientReviews, keyword) {
 
-    if(clientReviews==0) {
+    if (clientReviews.length == 0 || !clientReviews) {
         showMessage("reviews");
     }
     else {
 
-        showReviews(bookInfo["bookReviews"], keyword);
+        showReviews(clientReviews, keyword);
 
-          $("#search-book-reviews").on("input", () => searchBookReviews(bookInfo["bookReviews"], keyword));
+        $("#search-book-reviews").on("input", () => searchBookReviews(keyword));
 
-          $("#clear-book-reviews").on("click", () => {
-                    $("#search-book-reviews").val("");
-                    showReviews(bookInfo["bookReviews"], keyword)});
+        $("#clear-book-reviews").on("click", () => {
+            $("#search-book-reviews").val("");
+            showReviews(clientReviews, keyword)
+        });
 
     }
 
@@ -97,87 +90,97 @@ function showReviews(reviews, keyword) {
                             <p class="card-text">${review['review']}</p>
                         </div>
                         <div class="d-flex justify-content-end">
-                            ${(keyword == "info") ? 
-                                `<button class="btn btn-outline-secondary rounded-pill px-4" id='btn-details-reviews-${review['clientId']}'>Details</button>` :
-                                `<button class="btn btn-outline-danger rounded-pill px-4" id='btn-delete-reviews-${review['clientId']}'>Delete</button>`}
+                            ${(keyword == "info") ?
+                `<button class="btn btn-outline-secondary rounded-pill px-4" id='btn-details-reviews-${review['clientId']}'>Details</button>` :
+                `<button class="btn btn-outline-danger rounded-pill px-4" id='btn-delete-reviews-${review['clientId']}'>Delete</button>`}
                         </div>
                     </div>
                 </div>
             </div>`
         );
-        if(keyword=="info"){ 
-            $(`#btn-details-reviews-${review['clientId']}`).on("click", () => {  
-                detailsClientInfo(review['clientId'])} );
+        if (keyword == "info") {
+            $(`#btn-details-reviews-${review['clientId']}`).on("click", () => {
+                detailsClientInfo(review['clientId'])
+            });
         }
-        if(keyword=="edit"){
-            $(`#btn-delete-reviews-${review['clientId']}`).on("click", () => { 
-                deleteButtonEdit(review['clientID'], "review")});
+        if (keyword == "edit") {
+            $(`#btn-delete-reviews-${review['clientId']}`).on("click", () => {
+                deleteButtonEdit(review['clientID'], "review")
+            });
         }
     });
 
 };
 
+//Function to search review via APIs
+async function searchReviewsAPI(searchedKeyword) {
 
-// Function to make use of the search-bar for book reviews
-function searchBookReviews(reviews,keyword) {
-
-    const searchedKeyword = $("#search-book-reviews").val();
-    console.log("Keyword for reviews is: ", searchedKeyword);
-
-    const searchedReviews = searchBookReviewsHelper(reviews, searchedKeyword);
-
-    showReviews(searchedReviews, keyword);
-};
-
-
-// A helper function to search through reviews based on the keyword
-function searchBookReviewsHelper(reviews, keyword) {
-    if (keyword == "") {
-        return reviews;
-    } else {
-        let searchedReviews = [];
-
-        reviews.forEach(review => {;
-
-            const clientName = review["name"].toLowerCase();
-            const clientUsername = review["username"].toLowerCase();
-            const clientID = review["clientId"].toString();
-            const reviewText = review["review"].toLowerCase();
-            
-
-            if (clientName.includes(keyword.toLowerCase()) || 
-                clientUsername.includes(keyword.toLowerCase()) || 
-                clientID.includes(keyword.toLowerCase()) ||
-                reviewText.includes(keyword.toLowerCase())) {
-                searchedReviews.push(review);
-            }
+    const url = `http://localhost:5223/Book/search-book-reviews/${Number(JSON.parse(localStorage.getItem("selectedBook")))}/${searchedKeyword}`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
         });
 
-        console.log("Searched Reviews: ", searchedReviews);
-        return searchedReviews;
+        if (!response.ok) {
+            return [];
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Failed to fetch book cards:', error);
+        return [];
     }
+}
+
+
+// Function to make use of the search-bar for book reviews
+async function searchBookReviews(keyword) {
+
+    const searchedKeyword = $("#search-book-reviews").val().trim().toLowerCase();
+    console.log("Keyword for reviews is: ", searchedKeyword);
+
+    if (searchedKeyword == "") {
+        return;
+    }
+    const searchedReviews = await searchReviewsAPI(searchedKeyword);
+
+    if(searchedReviews.length == 0 || !searchedReviews)
+    {
+        $("#book-reviews").empty();
+        $(`#book-reviews`).append(
+            `<div class="col-12 mb-3 p-2">
+                 <h6 class="text-center" style="color: rgb(253, 96, 96);"> No Content on this part yet. :(</h6>
+             </div>`
+        );
+
+        return;
+    }
+
+    showBookReviews(searchedReviews, keyword);
 };
 
 
 //Function to deal with the WHOLE client-likes part
-function showBookLikes(clientLikes,bookInfo,keyword) {
+function showBookLikes(bookLikes, keyword) {
 
 
-    if(clientLikes==0) {
+    if (bookLikes == 0 || !bookLikes) {
 
         showMessage("likes");
         return;
     }
-    else
-    {
+    else {
 
-        showClients(bookInfo["bookLikes"],keyword);
+        showClients(bookLikes, keyword);
 
-        $("#search-book-likes").on("input", () => searchBookLikes(bookInfo["bookLikes"],keyword));
+        $("#search-book-likes").on("input", () => searchBookLikes(keyword));
 
-        $("#clear-book-likes").on("click" , () => {
+        $("#clear-book-likes").on("click", () => {
             $("#search-book-likes").val("")
-            showClients(bookInfo["bookLikes"],keyword);
+            showClients(bookLikes, keyword);
         });
     }
 
@@ -188,21 +191,21 @@ function showBookLikes(clientLikes,bookInfo,keyword) {
 function detailsBookInfo(bookInfo) {
 
     document.getElementById("book-image-container").innerHTML = `<img src="${bookInfo["coverImageUrl"]}" class="img-fluid rounded-4 border shadow-lg" alt="This is the book cover photo">`;
-    document.querySelector("#book-title-container").innerHTML=`<h3>${bookInfo["title"]}</h3>`;
-    document.querySelector("#book-description-container").innerHTML=`<p> ${bookInfo["description"]} </p>`;
-    document.querySelector("#book-info-author").innerHTML=`${bookInfo["author"]}`;
-    document.querySelector("#book-info-genre").innerHTML=`${bookInfo["genre"]} `;
-    document.querySelector("#book-info-year").innerHTML=` ${bookInfo["publishedYear"]} `;
-    document.querySelector("#book-info-price").innerHTML=`${bookInfo["price"]}$ `;
-    document.querySelector("#book-info-rating").innerHTML=`${bookInfo["rating"]} `;
-    document.querySelector("#book-info-likes").innerHTML=`${bookInfo["noLikes"]} `;
-    document.querySelector("#book-info-reviews").innerHTML=`${bookInfo["noReviews"]} `;
-    document.querySelector("#book-info-id").innerHTML=`${bookInfo["bookId"]} `;
+    document.querySelector("#book-title-container").innerHTML = `<h3>${bookInfo["title"]}</h3>`;
+    document.querySelector("#book-description-container").innerHTML = `<p> ${bookInfo["description"]} </p>`;
+    document.querySelector("#book-info-author").innerHTML = `${bookInfo["author"]}`;
+    document.querySelector("#book-info-genre").innerHTML = `${bookInfo["genre"]} `;
+    document.querySelector("#book-info-year").innerHTML = ` ${bookInfo["publishedYear"]} `;
+    document.querySelector("#book-info-price").innerHTML = `${bookInfo["price"]}$ `;
+    document.querySelector("#book-info-rating").innerHTML = `${bookInfo["rating"]} `;
+    document.querySelector("#book-info-likes").innerHTML = `${bookInfo["noLikes"]} `;
+    document.querySelector("#book-info-reviews").innerHTML = `${bookInfo["noReviews"]} `;
+    document.querySelector("#book-info-id").innerHTML = `${bookInfo["bookId"]} `;
 
     $("#delete-btn-info").click(deleteBookInfo);
 
     $("#edit-btn-info").on("click", () => {
-        selectBook(bookInfo["bookId"],"edit");
+        selectBook(bookInfo["bookId"], "edit");
     });
 
 };
@@ -218,8 +221,7 @@ function detailsClientInfo(id) {
 
 
 //Function to show message at Book-likes/reviews
-function showMessage(id) 
-{
+function showMessage(id) {
     $(`#book-${id}`).append(
         `<div class="col-12 mb-3 p-2">
              <h6 class="text-center" style="color: rgb(253, 96, 96);"> No Content on this part yet. :(</h6>
@@ -232,7 +234,7 @@ function showMessage(id)
 
 
 //function to show the clients who liked the book
-function  showClients(likedClients,keyword) {
+function showClients(likedClients, keyword) {
 
     $("#book-likes").empty();
 
@@ -247,11 +249,11 @@ function  showClients(likedClients,keyword) {
                             <p class="card-text text-muted">Username: ${clientInfo['username']}</p>
                             <p class="card-text text-muted">ID: ${clientInfo['clientId']}</p>
                         </div>
-                        ${(keyword=="info") ? 
-                            `<div class="d-flex align-items-center p-2">
+                        ${(keyword == "info") ?
+                `<div class="d-flex align-items-center p-2">
                             <button class="btn btn-outline-primary rounded-pill px-4" id='btn-details-likes-${clientInfo['clientId']}'>Details</button>
-                        </div> ` : 
-                        `<div class="d-flex align-items-center p-2">
+                        </div> ` :
+                `<div class="d-flex align-items-center p-2">
                             <button class="btn btn-outline-danger rounded-pill px-4" id='btn-delete-likes-${clientInfo['clientId']}'>Delete</button>
                         </div>` }
 
@@ -260,15 +262,16 @@ function  showClients(likedClients,keyword) {
             </div>`
         );
 
-        if(keyword=="info"){
-            $(`#btn-details-likes-${clientInfo['clientId']}`).on("click", () => {  
-
-                detailsClientInfo(clientInfo['clientId'])});
+        if (keyword == "info") {
+            $(`#btn-details-likes-${clientInfo['clientId']}`).on("click", () => {
+                detailsClientInfo(clientInfo['clientId'])
+            });
         };
 
-        if(keyword=="edit"){
-            $(`#btn-delete-likes-${clientInfo['clientId']}`).on("click", () => { 
-                deleteButtonEdit(bookInfo,clientInfo['ID'],clients, "client")}); 
+        if (keyword == "edit") {
+            $(`#btn-delete-likes-${clientInfo['clientId']}`).on("click", () => {
+                deleteButtonEdit(bookInfo, clientInfo['ID'], clients, "client")
+            });
         }
     });
 };
@@ -279,68 +282,82 @@ async function deleteBookInfo() {
 
     const agree = confirm("Are you sure you want to delete this Book?");
 
-    if(agree) {
+    if (agree) {
 
         const bookInfo = JSON.parse(localStorage.getItem("selectedBook"));
 
-        console.log("Consoling book Info",bookInfo);
+        console.log("Consoling book Info", bookInfo);
 
         const deleted = await deleteBookAPI(bookInfo)
 
-        if(deleted)
-        {
-            window.location.href="manager-book-management.html";
+        if (deleted) {
+            window.location.href = "manager-book-management.html";
         }
-        else
-        {
+        else {
             alert("Book not deleted");
         }
     }
 };
 
+//Function to search Likes via APIs
+async function searchLikesAPI(searchedKeyword) {
 
-//Function to make use of the search-bar
-function searchBookLikes(clients, keyword) {
-
-    const searchkeyword = $("#search-book-likes").val();
-
-    console.log("Keyword is: ", searchkeyword);
-
-    const searchedClients = searchBookLikesHelper(clients, searchkeyword);
-
-    showClients(searchedClients,keyword);
-};
-
-
-//A helper function for the searchBookLikes
-function searchBookLikesHelper(clients, searchkeyword) {
-
-    if (searchkeyword == "") {
-        return clients;
-    }
-    else {
-        let searchedClients = [];
-
-        clients.forEach(client => {
-
-            if (client["name"].toLowerCase().includes(searchkeyword.toLowerCase())
-                || client["username"].toLowerCase().includes(searchkeyword.toLowerCase())
-                || JSON.stringify(client["clientId"]).toLowerCase().includes(searchkeyword.toLowerCase())) {
-                searchedClients.push(client);
-            }
+    const url = `http://localhost:5223/Book/search-book-likes/${Number(JSON.parse(localStorage.getItem("selectedBook")))}/${searchedKeyword}`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
         });
 
-        console.log("Searched Clients are: ", searchedClients);
+        if (!response.ok) {
+            return [];
+        }
 
-        return searchedClients;
+        return await response.json();
+    } catch (error) {
+        console.error('Failed to fetch book cards:', error);
+        return [];
     }
+}
+
+
+//Function to make use of the search-bar
+async function searchBookLikes(keyword) {
+
+    const searchKeyword = $("#search-book-likes").val().trim().toLowerCase();
+
+    console.log("Keyword is: ", searchKeyword);
+
+
+    if (searchKeyword == "") {
+        return;
+    }
+
+    const searchedLikes = await searchLikesAPI(searchKeyword);
+
+    if(searchedLikes.length == 0 || !searchedLikes)
+    {
+        $("#book-likes").empty();
+        $(`#book-likes`).append(
+            `<div class="col-12 mb-3 p-2">
+                 <h6 class="text-center" style="color: rgb(253, 96, 96);"> No Content on this part yet. :(</h6>
+             </div>`
+        );
+
+        return;
+    }
+
+    showBookLikes(searchedLikes, keyword);
 };
 
-//Function to give functionality to the delete buttons in the edit page
-function deleteButtonEdit(book,clientID,clients, keyword) {
 
-    if(keyword=="client") {
-        
+//Function to give functionality to the delete buttons in the edit page
+function deleteButtonEdit(book, clientID, clients, keyword) {
+
+    if (keyword == "client") {
+
         book["Likes-clients"] = book["Likes-clients"].filter(id => id != clientID);
 
         localStorage.setItem("selectedBook", JSON.stringify(book));
@@ -358,10 +375,9 @@ function deleteButtonEdit(book,clientID,clients, keyword) {
 
         clients.forEach(client => {
 
-            if(client["ID"] == clientID)
-                {
-                    client["Books-liked"]= client["Books-liked"].filter(id => id != book["ID"] );
-                }
+            if (client["ID"] == clientID) {
+                client["Books-liked"] = client["Books-liked"].filter(id => id != book["ID"]);
+            }
         });
 
         localStorage.setItem("client", JSON.stringify(clients));
@@ -370,40 +386,40 @@ function deleteButtonEdit(book,clientID,clients, keyword) {
 
         location.reload();
 
-        showClients(book["Likes-clients"],keyword, book);
+        showClients(book["Likes-clients"], keyword, book);
     }
 
     if (keyword == "review") {
 
         book["Reviews"] = book["Reviews"].filter(review => review["clientID"] != clientID);
-    
+
         localStorage.setItem("selectedBook", JSON.stringify(book));
-    
+
         let books = JSON.parse(localStorage.getItem("book"));
-    
+
         books = books.map((newBook) => {
             if (newBook["ID"] === book["ID"]) {
                 return { ...book };
             }
             return newBook;
         });
-    
+
         localStorage.setItem("book", JSON.stringify(books));
-    
+
         alert("Review Deleted");
-    
+
         location.reload();
-    
+
         showReviews(book["Reviews"], clients, keyword, book);
     }
 
 };
 
 //The actual function callings
-$(document).ready(function() {
-    document.getElementById("logout-button").addEventListener("click", logoutAction);  
+$(document).ready(function () {
+    document.getElementById("logout-button").addEventListener("click", logoutAction);
     showBookInfo();
 
 });
 
-export {showBookInfo,deleteBookInfo, showBookLikes, showBookReviews};
+export { showBookInfo, deleteBookInfo, showBookLikes, showBookReviews };
